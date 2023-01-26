3e79699558fa179a7b58e8cee4c06ddda2d1fedf
DerrickSaltFish <1049347983@qq.com>
2023-01-26 15:24:53 +0800

    The Level-1 version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner in = new Scanner(System.in);
+        String line = "     ____________________________________________________________     \n";
+        System.out.println(line + "     Hello! I'm Duke\n" + "     What can I do for you?\n" + line);
+        String input = in.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println(line + "     " + input + "\n" + line);
+            input = in.nextLine();
+        }
+        System.out.println(line + "     Bye. Hope to see you again soon!" + "\n" + line);
     }
 }
