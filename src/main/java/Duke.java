db02b4546501d3566230b4a445c64e86e92a721d
DerrickSaltFish <1049347983@qq.com>
2023-02-09 00:14:20 +0800

    Level-2 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -13,9 +15,28 @@ public class Duke {
         System.out.println(line + "     Hello! I'm Duke\n" + "     What can I do for you?\n" + line);
         String input = in.nextLine();
         while(!input.equals("bye")) {
-            System.out.println(line + "     " + input + "\n" + line);
-            input = in.nextLine();
+            if(input.equals("list")) {
+                System.out.println(line);
+                for(int i = 0; i < 100; i++) {
+                    if(tasks[i] != null) {
+                        System.out.println("     " + (i + 1) + "." + tasks[i]);
+                    } else{
+                        break;
+                    }
+                }
+                System.out.println(line);
+            } else {
+                for(int i = 0; i < 100; i++) {
+                    if(tasks[i] == null) {
+                        tasks[i] = input;
+                        break;
+                    }
+                }
+                System.out.println(line + "     added: " + input + "\n" + line);
+            }
+                input = in.nextLine();
         }
+
         System.out.println(line + "     Bye. Hope to see you again soon!" + "\n" + line);
     }
 }
